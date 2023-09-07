ed30faf77875645f8e5f05ebd0bd164fff9f2677
hyper <mohamedaharoun@gmail.com>
2023-09-08 01:01:14 +0300

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-/*betty my a$$*/
-int main(void);
+#include <stdio.h>
+/**
+ * betty my a$$
+*/
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
