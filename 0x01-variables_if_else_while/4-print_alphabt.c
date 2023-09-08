07faa3549ebe14789a64d85bd982618bac5444f7
hyper <mohamedaharoun@gmail.com>
2023-09-09 01:27:07 +0300

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,20 +8,15 @@
 */
 int main(void)
 {
-
-
-
-
 char l = 'a';
-while (l <= 'z') {
-if (l != 'q' && l != 'e') {
+while (l <= 'z')
+{
+if (l != 'q' && l != 'e')
+{
 putchar(l);
 }
 l++;
 }
-
-
-
 putchar('\n');
 return (0);
 }
