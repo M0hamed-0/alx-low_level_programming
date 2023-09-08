36d99028c96d131b13e5129336c9610dd87764f2
hyper <mohamedaharoun@gmail.com>
2023-09-09 01:14:53 +0300

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Description: Description
+ *
+ * Return: Return
+*/
+int main(void)
+{
+char l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l++;
+}
+char L = 'A';
+while (L <= 'Z')
+{
+putchar(L);
+L++;
+}
+putchar('\n');
+return (0);
+}
