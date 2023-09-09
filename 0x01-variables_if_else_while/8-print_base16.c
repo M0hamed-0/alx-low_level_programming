ca65b5aeea1954b743871505f7f51b4378a4106f
hyper <mohamedaharoun@gmail.com>
2023-09-09 03:32:37 +0300

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-
-int main(void) 
+/**
+ * main - main
+ *
+ * Description: Description
+ *
+ * Return: Return
+*/
+int main(void)
 {
 char d;
-for (d = '0'; d <= '9'; d++) {
+for (d = '0'; d <= '9'; d++)
+{
 putchar(d);
 }
-for (d = 'a'; d <= 'f'; d++) {
+for (d = 'a'; d <= 'f'; d++)
+{
 putchar(d);
 }
 putchar('\n');
