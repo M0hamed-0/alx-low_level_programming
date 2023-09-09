934da102982e68248f9806a901bbeb2129acd065
hyper <mohamedaharoun@gmail.com>
2023-09-09 03:38:40 +0300

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,16 @@
 int main(void) 
 {
 int digit = 0;
-while (digit < 10) {
-putchar(digit + '0');     
-if (digit < 9) {
+while (digit < 10)
+{
+putchar(digit + '0');  
+if (digit < 9)
+{
 putchar(',');
 putchar(' ');
 }
 digit++;
 }
 putchar('\n');
-return 0;
+return (0);
 }
