7fb24e9a24a086f34c93fa03c59f8197f5418e0d
hyper <mohamedaharoun@gmail.com>
2023-09-09 03:16:42 +0300

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Description: Description
+ *
+ * Return: Return
+*/
+int main(void)
+{
+char characters[26];
+char l = 'a';
+int i = 0;
+while (l <= 'z')
+{
+characters[i] = l;
+i++;
+l++;
+}
+{
+for (int j = i - 1; j >= 0; j--) {
+putchar(characters[j]);
+}
+putchar('\n');
+return (0);
+}
+}
