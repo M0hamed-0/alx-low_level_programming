09d4281f556188e5240d8036649bbc6dc544a287
hyper <mohamedaharoun@gmail.com>
2023-09-09 04:11:06 +0300

    13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
 /**
  * main - ssss
  *
@@ -9,7 +11,7 @@
 int main(void) 
 {
 int i, j;
-for (i = 0; i <= 99; i++)
+for (i = 0; i <= 98; i++)
 {
 for (j = 0; j <= 99; j++)
 {
@@ -18,7 +20,7 @@ putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if (i != 99 || j != 99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
