3dfe85922aece0c3a6d253db2e9d6f0218567c37
hyper <mohamedaharoun@gmail.com>
2023-09-09 04:00:54 +0300

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,21 @@
 */
 int main(void)
 {
-int tens1, ones1, tens2, ones2;
-
-for (tens1 = 0; tens1 <= 9; tens1++)
+int num1_tens, num1_ones, num2_tens, num2_ones;
+for (num1_tens = 0; num1_tens <= 9; num1_tens++)
 {
-for (ones1 = 0; ones1 <= 9; ones1++)
+for (num1_ones = 0; num1_ones <= 9; num1_ones++)
 {
-for (tens2 = tens1; tens2 <= 9; tens2++)
+for (num2_tens = num1_tens; num2_tens <= 9; num2_tens++)
 {
-int start_ones2 = (tens1 == tens2) ? ones1 + 1 : 0;
-for (ones2 = start_ones2; ones2 <= 9; ones2++)
+for (num2_ones = (num1_tens == num2_tens) ? num1_ones + 1 : 0; num2_ones <= 9; num2_ones++)
 {
-putchar('0' + tens1);
-putchar('0' + ones1);
+putchar(num1_tens + '0');
+putchar(num1_ones + '0');
 putchar(' ');
-putchar('0' + tens2);
-putchar('0' + ones2);
-
-if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9))
+putchar(num2_tens + '0');
+putchar(num2_ones + '0');
+if (!(num1_tens == 9 && num1_ones == 9 && num2_tens == 9 && num2_ones == 9))
 {
 putchar(',');
 putchar(' ');
@@ -34,8 +31,6 @@ putchar(' ');
 }
 }
 }
-
 putchar('\n');
-
-return (0);
+return 0;
 }
