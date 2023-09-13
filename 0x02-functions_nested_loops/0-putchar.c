1b484a39ead706584ce4445a9a56a1d64fd4376e
hyper <mohamedaharoun@gmail.com>
2023-09-13 03:03:58 +0300

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
- * main: Entry point of the program
+ * main - Entry point of the program
  *
  * Description: This function is the entry point of the program.
  * It prints "_putchar" to the standard output.
  *
  * Return: Always 0 (success)
 */
-
 int main(void)
 {
 char str[] = "_putchar";
