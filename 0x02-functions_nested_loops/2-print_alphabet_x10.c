8217f251103f5e1a8428b59bd06e2b99a074d5eb
hyper <mohamedaharoun@gmail.com>
2023-09-13 03:29:53 +0300

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - his fucntion prints the alphabet from a to z
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int x;
+int ch;
+for (x = 1; x <= 10; x++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+
+_putchar(ch);
+_putchar('\n');
+}
+}
+}
