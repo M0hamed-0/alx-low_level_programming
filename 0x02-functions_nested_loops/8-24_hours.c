c94c02f4adfeacb0c889d439c8c7f688e18ce762
hyper <mohamedaharoun@gmail.com>
2023-09-13 05:23:25 +0300

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - his fucntion prints the alphabet from a to z
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int m, h;
+for (h = 0; h <= 23; h++)
+{
+for (m= 0; m < 59; m++)
+{
+-putchar((h / 10) + 48);
+-putchar((h % 10) + 48);
+-putchar(':');
+-putchar((m / 10) + 48);
+-putchar((m % 10) + 48);
+-putchar('\n');
+}
+}
+}
