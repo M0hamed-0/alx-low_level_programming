cf1225852c8b23c997981852676206d247c3ae31
hyper <mohamedaharoun@gmail.com>
2023-09-13 04:09:24 +0300

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - his fucntion prints the alphabet from a to z
+ *
+ * @c: edfwfef
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+if (c >= 65 && c <= 90)
+return (1);
+return (0);
+}
