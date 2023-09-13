ae94419d86e33763096a788337c81a36c2c344b3
hyper <mohamedaharoun@gmail.com>
2023-09-13 04:24:09 +0300

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,13 +11,13 @@
 int print_sign(int n)
 {
 if(n > 0)
-return (+1);
+return ('1');
 _putchar('+');
 if(n == 0)
-return (0);
+return ('00');
 _putchar('0');
 if(n < 0)
-return (-1);
+return ('-1');
 _putchar('-');
 return (0);
 }
