5615bcf08f29bc03be152d21fc4d2b9828a6f354
hyper <mohamedaharoun@gmail.com>
2023-09-14 00:14:42 +0300

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - his fucntion prints the alphabet from a to z
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+int (int y, int x)
+{
+int z = y + x;
+return (z);
+}
