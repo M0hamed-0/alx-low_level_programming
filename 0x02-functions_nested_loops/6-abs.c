e837c0b21c6546771ec5fc536472b1775fad7b9e
hyper <mohamedaharoun@gmail.com>
2023-09-13 04:41:54 +0300

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - his fucntion prints the alphabet from a to z
+ *
+ * @n: edfwfef
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+if(n > 0);
+n = (-1) * n;
+return (n);
+}
