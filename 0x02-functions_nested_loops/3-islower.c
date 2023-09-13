81aa657e0e2bbc1c18c3ea1a794ee81f968db7b5
hyper <mohamedaharoun@gmail.com>
2023-09-13 03:47:52 +0300

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - his fucntion prints the alphabet from a to z
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (c <= 97 && c >= 122)
+{
+return(1);
+}
+return(0);
+}
