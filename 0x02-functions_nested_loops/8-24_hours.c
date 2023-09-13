385fa688150cfa8df24f7cc389b76e0014b5895a
hyper <mohamedaharoun@gmail.com>
2023-09-13 05:35:01 +0300

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 int min, hr;
 for (hr = 0; hr <= 23; hr++)
 {
-for (min= 0; min <= 59; min++)
+for (min = 0; min <= 59; min++)
 {
 _putchar((hr / 10) + '0');
 _putchar((hr % 10) + '0');
