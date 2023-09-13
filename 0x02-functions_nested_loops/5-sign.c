f006708e0ed10e78193180ab9425649dbf5c0bf0
hyper <mohamedaharoun@gmail.com>
2023-09-13 04:18:20 +0300

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - his fucntion prints the alphabet from a to z
+ *
+ * @c: edfwfef
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+if(n > 0)
+return (1);
+_putchar('+');
+if(n = 0)
+return (0);
+_putchar('0');
+if(n < 0)
+return (-1);
+_putchar('-');
+}
