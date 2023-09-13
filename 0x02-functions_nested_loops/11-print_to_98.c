bec9f513e661bd84d0a171beac1aad0fdfafa924
hyper <mohamedaharoun@gmail.com>
2023-09-14 01:00:23 +0300

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - his fucntion prints the alphabet from a to z
+ *
+ * @n: blablabla
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+for (n = n; n <= 98; n++)
+{
+    printf("%d, ");
+}
+printf("\n");
+return (0);
+}
