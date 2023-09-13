e2e54ec82b05dfa4e6613451ff7fd99fafba1312
hyper <mohamedaharoun@gmail.com>
2023-09-13 05:51:02 +0300

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,8 +2,6 @@
 /**
  * times_table - his fucntion prints the alphabet from a to z
  *
- * @n: edfwfef
- *
  * Description: this fucntion prints the alphabet from a to z
  *
  * Return: Always 0.
@@ -13,19 +11,19 @@ void times_table(void)
 int n, m, p;
 for (n = 0; n <= 9; n++)
 {
-    _putchar('0');
-    for (m = 1; m <= 9; n++)
-    {
-        _putchar('.');
-        _putchar(' ');
-        p = n * m;
-        if (p <= 9)
-            _putchar(' ');
-        else
-            _putchar((p / 10) + '0');
-        _putchar((p % 10) + '0'); 
-    }
-    _putchar('\n');
+_putchar('0');
+for (m = 1; m <= 9; n++)
+{
+_putchar('.');
+_putchar(' ');
+p = n * m;
+if (p <= 9)
+_putchar(' ');
+else
+_putchar((p / 10) + 48);
+_putchar((p % 10) + 48); 
+}
+_putchar('\n');
 }
 }
 
