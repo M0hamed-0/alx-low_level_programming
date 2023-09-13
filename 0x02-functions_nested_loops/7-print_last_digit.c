3391be87541d2f5eac94a7c4944fa3b1ecd8aa0c
hyper <mohamedaharoun@gmail.com>
2023-09-13 04:52:06 +0300

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - his fucntion prints the alphabet from a to z
+ *
+ * @n: edfwfef
+ *
+ * Description: this fucntion prints the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+int LD;
+LD = n % 10;
+printf("%d",LD);
+}
