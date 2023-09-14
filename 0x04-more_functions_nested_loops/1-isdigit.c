cd2049da9279773d69009da0413aa325e093d476
hyper <mohamedaharoun@gmail.com>
2023-09-14 13:22:59 +0300

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isdigit - blablabla
+ *
+ * @c: blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
