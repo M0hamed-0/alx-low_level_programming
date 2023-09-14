ff82f64647eb73f2f6f6bc08382e5240fa3fa3dd
hyper <mohamedaharoun@gmail.com>
2023-09-14 14:05:29 +0300

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - blablabla
+ *
+ * @b: blablabla
+ *
+ * @a: blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+int mul(int a, int b)
+{
+int s;
+s = a * b;
+return (s);
+}
