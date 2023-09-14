075691c282ec1f1318d66b66444bd3d40631ea99
hyper <mohamedaharoun@gmail.com>
2023-09-15 00:25:00 +0300

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+void print_most_numbers(void)
+{
+int x = 48;
+while (x <= 57)
+{
+if (x != 2 && x != 4)
+{
+_putchar(x);
+x++;
+}
+}
+_putchar('\n');
+}
