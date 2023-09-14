dcee6de3740ecfb890534ed4c71f0355ffd3424f
hyper <mohamedaharoun@gmail.com>
2023-09-14 08:09:35 +0300

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - blablabla
+ *
+ * @c: blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (1);
+}
