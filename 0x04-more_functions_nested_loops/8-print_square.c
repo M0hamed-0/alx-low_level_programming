6b36f8e993a277ddfec26ae8af4d8ab8ec29a152
hyper <mohamedaharoun@gmail.com>
2023-09-15 05:16:50 +0300

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - blablabla
+ *
+ * @size: blablabla
+ *
+ * Description: blablabla
+ *
+ * Return: blablabla
+ */
+void print_square(int size)
+{
+int x, y;
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
