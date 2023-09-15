43073baf72ad7b3bd27e0e5fbabde6e5224eeb14
hyper <mohamedaharoun@gmail.com>
2023-09-15 05:54:12 +0300

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
-void main()
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-
-
-
-
-
-    
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i < 100)
+{
+printf(" ");
+}
+else
+{
+printf("\n");
+}
+}
+return (0);
 }
