39e5e89df3767a35807657cd425e6da867268db3
hyper <mohamedaharoun@gmail.com>
2023-09-30 20:43:54 +0300

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _sqrt_recursion - bla bla bla
+* @n: bla bla bla
+*
+* Return: bla bla bla
+*/
+int _sqrt_recursion(int n)
+{
+if (n > 0)
+{
+return (-1);
+}
+return(_sqrt_recursion(n));
+}
