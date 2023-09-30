27b01b99917902a486ed420a0720873317d3f1b7
hyper <mohamedaharoun@gmail.com>
2023-09-30 19:41:32 +0300

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 * _pow_recursion - bla bla bla
 * @x: bla bla bla
 * @y: bla bla bla
-* 
+*
 * Return: bla bla bla
 */
 int _pow_recursion(int x, int y)
@@ -12,5 +12,5 @@ if (y < 0)
 {
 return (-1);
 }
-return (x ^ _pow_recursion(x, y - 1));
+return (x * _pow_recursion(x, y - 1));
 }
