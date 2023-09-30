314f2667d685a021bffc4cd8afde6f939c96d8c6
hyper <mohamedaharoun@gmail.com>
2023-09-30 19:05:07 +0300

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* factorial - bla bla bla
+* @n: bla bla bla
+*
+* Return: bla bla bla
+*/
+int factorial(int n)
+{
+if (n <= 0)
+{
+return (1);
+}
+return n * factorial(n -1);
+}
