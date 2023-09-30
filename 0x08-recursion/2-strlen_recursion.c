b2c3ac087bba9e5ec371a936d9889a6868488f02
hyper <mohamedaharoun@gmail.com>
2023-09-30 18:45:28 +0300

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _print_rev_recursion - bla bla bla
+* _strlen_recursion - bla bla bla
 * @s: bla bla bla
 */
 int _strlen_recursion(char *s)
@@ -8,7 +8,8 @@ int _strlen_recursion(char *s)
 int SL = 0;
 if (*s)
 {
-    SL++;
+SL++;
+SL += _strlen_recursion(s + 1);
 }
 return SL;
 }
