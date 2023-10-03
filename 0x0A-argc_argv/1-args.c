dec703b47ea281926fb20809434bde725076494a
hyper <mohamedaharoun@gmail.com>
2023-10-03 04:36:13 +0300

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - bla bla bla
+ * @argc: bla bla bla
+ * @argv: bla bla bla
+ * Return: bla
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+printf("%d\n", argc);
+}
