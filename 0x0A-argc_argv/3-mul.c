0b950b467a5cfe20386bd13f9285a5b4cba156f5
hyper <mohamedaharoun@gmail.com>
2023-10-03 05:18:50 +0300

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - bla bla bla
+ * @argc: bla bla bla
+ * @argv: bla bla bla
+ *
+ * Return: bla
+ */
+int main(int argc, char **argv)
+{
+int sum;
+if (argc > 3)
+{
+printf("Error");
+}
+else
+{
+sum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", sum);
+}
+return (0);
+}
