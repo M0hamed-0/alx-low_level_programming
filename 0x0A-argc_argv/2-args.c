5a1ad25f9f1b5692a0a09eecbf86afdd158f1f97
hyper <mohamedaharoun@gmail.com>
2023-10-03 05:08:08 +0300

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,15 +4,15 @@
  * main - bla bla bla
  * @argc: bla bla bla
  * @argv: bla bla bla
- * @i: bla
+ *
  * Return: bla
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 int i;
 for (i = 0; i < argc; i++)
 {
-    printf("%s\n", argv[i]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
