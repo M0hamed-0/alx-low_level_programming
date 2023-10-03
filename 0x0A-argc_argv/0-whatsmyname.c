33ca85662a2a171f8c506aa8ff763f9ae9423490
hyper <mohamedaharoun@gmail.com>
2023-10-03 04:17:05 +0300

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,8 @@
  * main - bla bla bla
  *
  */
-int main(int __attribute__((unused)) argc, char* argv[])
+int main(int __attribute__((unused)) argc, char* __attribute__((unused)) argv[])
 {
 printf("%s\n", argv[0]);
+return (0);
 }
