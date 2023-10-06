b66298e13c8ea511a72c93a424b40c3f7b2ec882
hyper <mohamedaharoun@gmail.com>
2023-10-06 04:26:07 +0300

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,6 @@ unsigned int a;
 unsigned int b;
 unsigned int c;
 char *z;
-c = x + y;
 
 if (s1 == NULL)
 return (NULL);
@@ -33,7 +32,9 @@ for (y = 0; s2[y] != '\0'; y++)
 ;
 }
 
-z = malloc(x + y + 1);
+c = x + y;
+
+z = malloc(c + 1);
 if (z == NULL)
 return (NULL);
 
@@ -43,7 +44,7 @@ z[a] = s1[a];
 for (b = x; b < c; b++)
 z[a + b] = s1[a + b];
 
-z[x] = '\0';
+z[c] = '\0';
 
 return (z);
 }
