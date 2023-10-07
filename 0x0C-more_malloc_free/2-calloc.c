87f09c19c9f165ef07d15040d681149800ad9b50
hyper <mohamedaharoun@gmail.com>
2023-10-08 02:21:05 +0300

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_calloc - bla bla bla
+ * @nmemb: bla bla bla
+ * @size: bla bla bla
+ * Return: bla
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	unsigned int *x;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (nmemb == 0)
+	{
+		return (NULL);
+	}
+
+
+	x = malloc(nmemb * size);
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+
+
+	for (i = 0; i < size; i++)
+	{
+		x[i] = '0';
+	}
+	return (x);
+
+}
