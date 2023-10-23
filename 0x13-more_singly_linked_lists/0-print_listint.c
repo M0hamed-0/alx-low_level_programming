fa3c71a46aa16a559ba03e70811f501981ab1372
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-10-24 00:03:52 +0300

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_listint - prints bla bla
+ * @h: head
+ * Return: n of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count;
+	const listint_t *ptr = NULL;
+
+		if (h == NULL)
+	{
+		return (0);
+	}
+
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
