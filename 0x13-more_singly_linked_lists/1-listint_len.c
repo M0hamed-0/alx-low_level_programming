5adda82c95f680e537928f096b7eca48fc0c19e7
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-10-24 02:24:47 +0300

    1,1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * list_len - function that returns the number of
+ * listint_len - function that returns the number of
  * elements in a linked list_t list.
  * @h: pointer to struct
  * Return: the number of nodes
 */
-size_t list_len(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int count = 0;
 	const listint_t *ptr = NULL;
