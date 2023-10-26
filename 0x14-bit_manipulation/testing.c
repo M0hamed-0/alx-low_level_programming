a586952b97047e6335072b8f8f4937af6ac4316d
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-10-27 01:12:56 +0200

    2.4

--- 0x14-bit_manipulation/testing.c
+++ 0x14-bit_manipulation/testing.c
@@ -2,29 +2,7 @@
 
 int main(void)
 {
-	unsigned long int n;
-	int shift = (sizeof(unsigned long int) * 8) - 1;
-	int flag = 0;
-	int buffer[sizeof(unsigned int)];
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	while (shift >= 0)
-	{
-		if ((n >> shift) & 1)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		else if (flag == 1)
-		{
-			_putchar('0');
-		}
-		shift--;
-	}
+	printf("%d", sizeof(unsigned long int));
 
 }
