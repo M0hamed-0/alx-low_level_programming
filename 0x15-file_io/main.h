850cfe71cf2f54e9bda381c811d78935e4806fa5
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-10-31 23:14:18 +0200

    wth

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include <elf.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
